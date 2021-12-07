c866a99714ae72548a31b31bb0a6baea14178ff7
Marlaqk <ralsch@pm.me>
2021-12-07 08:35:12 +0100

    refactor day-06

--- day-06/code.py
+++ day-06/code.py
@@ -2,29 +2,24 @@ import fileinput
 from collections import defaultdict
 import re
 
+def calc(fish, days):
+    nrFishes = defaultdict(int)
+    for f in fish:
+        nrFishes[f] += 1
+
+    for _ in range(days):
+        spawnNr = nrFishes[0]
+        for x in range(1,9):
+            nrFishes[x-1] = nrFishes[x]
+        nrFishes[8] = spawnNr
+        nrFishes[6] += spawnNr
+
+    return sum(x for x in nrFishes.values())
+
+
 fish = list(map(int, re.findall(r'\d+', list(fileinput.input())[0])))
 
 # part 1
-for day in range(80):
-    for x in range(len(fish)):
-        if fish[x] == 0:
-            fish[x] = 6
-            fish.append(8)
-        else:
-            fish[x] -= 1
-
-print(len(fish))
-
+print(calc(fish, 80))
 # part 2
-nrFishes = defaultdict(int)
-for f in fish:
-    nrFishes[f] += 1
-
-for day in range(80, 256):
-    spawnNr = nrFishes[0]
-    for x in range(1,9):
-        nrFishes[x-1] = nrFishes[x]
-    nrFishes[8] = spawnNr
-    nrFishes[6] += spawnNr
-
-print(sum(x for x in nrFishes.values()))
+print(calc(fish, 256))
